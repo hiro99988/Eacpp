dab69e230e9243cdfbc993f57f5699d59d356039
hiro99988 <hrk9230.h@gmail.com>
2024-07-04 16:35:13 +0900

    =0のつけ忘れを直した

--- include/Rng/IRng.h
+++ include/Rng/IRng.h
@@ -10,17 +10,17 @@
 namespace Eacpp {
 
 struct IRng {
-    virtual int Integer(const int max) const;
-    virtual int Integer(int min, int max) const;
-    virtual std::vector<int> Integers(int min, int max, const int size, bool replace = true) const;
+    virtual int Integer(const int max) const = 0;
+    virtual int Integer(int min, int max) const = 0;
+    virtual std::vector<int> Integers(int min, int max, const int size, bool replace = true) const = 0;
 
-    virtual double Uniform(double min, double max) const;
-    virtual Eigen::ArrayXd Uniform(double min, double max, const int size) const;
-    virtual Eigen::ArrayXXd Uniform(double min, double max, const std::tuple<int, int> size) const;
+    virtual double Uniform(double min, double max) const = 0;
+    virtual Eigen::ArrayXd Uniform(double min, double max, const int size) const = 0;
+    virtual Eigen::ArrayXXd Uniform(double min, double max, const std::tuple<int, int> size) const = 0;
 
-    virtual double Random() const;
-    virtual Eigen::ArrayXd Random(const int size) const;
-    virtual Eigen::ArrayXXd Random(const std::tuple<int, int> size) const;
+    virtual double Random() const = 0;
+    virtual Eigen::ArrayXd Random(const int size) const = 0;
+    virtual Eigen::ArrayXXd Random(const std::tuple<int, int> size) const = 0;
 
     // template <typename T>
     // virtual Eigen::ArrayXX<T> Choice(Eigen::ArrayXX<T>, int size, bool replace = true) const;
