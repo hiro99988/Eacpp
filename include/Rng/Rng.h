e18855319e105b8bef9882efac99006cf64bf575
hiro99988 <hrk9230.h@gmail.com>
2024-07-04 19:40:36 +0900

    IRngを継承するように

--- include/Rng/Rng.h
+++ include/Rng/Rng.h
@@ -7,27 +7,29 @@
 #include <tuple>
 #include <vector>
 
+#include "Rng/IRng.h"
+
 namespace Eacpp {
 
-class Rng {
+class Rng : public IRng {
    public:
     Rng() : mt(std::random_device()()){};
     Rng(std::uint_fast32_t seed) : mt(seed){};
 
-    int Integer(const int max) const;
-    int Integer(int min, int max) const;
-    std::vector<int> Integers(int min, int max, const int size, bool replace = true) const;
+    int Integer(const int max) const override;
+    int Integer(int min, int max) const override;
+    std::vector<int> Integers(int min, int max, const int size, bool replace) const override;
 
-    double Uniform(double min, double max) const;
-    Eigen::ArrayXd Uniform(double min, double max, const int size) const;
-    Eigen::ArrayXXd Uniform(double min, double max, const std::tuple<int, int> size) const;
+    double Uniform(double min, double max) const override;
+    Eigen::ArrayXd Uniform(double min, double max, const int size) const override;
+    Eigen::ArrayXXd Uniform(double min, double max, const std::tuple<int, int> size) const override;
 
-    double Random() const;
-    Eigen::ArrayXd Random(const int size) const;
-    Eigen::ArrayXXd Random(const std::tuple<int, int> size) const;
+    double Random() const override;
+    Eigen::ArrayXd Random(const int size) const override;
+    Eigen::ArrayXXd Random(const std::tuple<int, int> size) const override;
 
     template <typename T>
-    Eigen::ArrayXX<T> Choice(Eigen::ArrayXX<T>, int size, bool replace = true) const;
+    Eigen::ArrayXX<T> Choice(Eigen::ArrayXX<T>, int size, bool replace) const;
 
    private:
     std::mt19937 mt;
