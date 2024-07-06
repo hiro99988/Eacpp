5191fc278b24b4ad77ef8796281b80efd95a1f3b
hiro99988 <hrk9230.h@gmail.com>
2024-07-06 17:46:22 +0900

    ちゃんと重複なしのint配列を生成できるように

--- src/Rng/Rng.cpp
+++ src/Rng/Rng.cpp
@@ -8,47 +8,47 @@
 
 namespace Eacpp {
 
-int Rng::Integer(const int max) const { return Integer(0, max); }
+int Rng::Integer(const int max) { return Integer(0, max); }
 
-int Rng::Integer(int min, int max) const {
+int Rng::Integer(int min, int max) {
     swapIfMaxLessThanMin(min, max);
     std::uniform_int_distribution<int> dist(min, max);
-    return dist(mt);
+    return dist(_mt);
 }
 
-std::vector<int> Rng::Integers(int min, int max, const int size, bool replace) const {
+std::vector<int> Rng::Integers(int min, int max, const int size, bool replace) {
     swapIfMaxLessThanMin(min, max);
     std::vector<int> result;
-    std::uniform_int_distribution<int> dist(min, max);
+    result.reserve(size);
     if (replace) {
+        std::uniform_int_distribution<int> dist(min, max);
         for (int i = 0; i < size; i++) {
-            result.push_back(dist(mt));
+            result.push_back(dist(_mt));
         }
         return result;
     } else {
         if (size > (max - min + 1)) {
             throw std::invalid_argument("Size must be less than or equal to the range of values");
         }
-        using hash_map = std::unordered_map<int, int>;
+        using hashMap = std::unordered_map<int, int>;
 
-        result.reserve(size);
+        hashMap map;
 
-        hash_map Map;
-
-        for (size_t cnt = 0; cnt < size; ++cnt) {
-            int val = dist(mt);
-            hash_map::iterator itr = Map.find(val);
+        for (size_t i = 0; i < size; ++i) {
+            int val = std::uniform_int_distribution<int>(min, max)(_mt);
+            hashMap::iterator itr = map.find(val);
 
             int replaced_val;
-            hash_map::iterator replaced_itr = Map.find(max);
-            if (replaced_itr != Map.end())
+            hashMap::iterator replaced_itr = map.find(max);
+            if (replaced_itr != map.end()) {
                 replaced_val = replaced_itr->second;
-            else
+            } else {
                 replaced_val = max;
+            }
 
-            if (itr == Map.end()) {
+            if (itr == map.end()) {
                 result.push_back(val);
-                if (val != max) Map.insert(std::make_pair(val, replaced_val));
+                if (val != max) map.insert(std::make_pair(val, replaced_val));
             } else {
                 result.push_back(itr->second);
                 itr->second = replaced_val;
@@ -61,29 +61,29 @@ std::vector<int> Rng::Integers(int min, int max, const int size, bool replace) c
     }
 }
 
-double Rng::Uniform(double min, double max) const {
-    if (min > max) {
-        std::swap(min, max);
-    }
+double Rng::Uniform(double min, double max) {
+    swapIfMaxLessThanMin(min, max);
     std::uniform_real_distribution<double> dist(min, max);
-    return dist(mt);
+    return dist(_mt);
 }
 
-Eigen::ArrayXd Rng::Uniform(double min, double max, const int size) const {
+Eigen::ArrayXd Rng::Uniform(double min, double max, const int size) {
+    swapIfMaxLessThanMin(min, max);
     std::uniform_real_distribution<double> dist(min, max);
-    Eigen::ArrayXd result = Eigen::ArrayXd::Zero(size).unaryExpr([&](double) { return dist(mt); });
+    Eigen::ArrayXd result = Eigen::ArrayXd::Zero(size).unaryExpr([&](double) { return dist(_mt); });
     return result;
 }
 
-Eigen::ArrayXXd Rng::Uniform(double min, double max, const std::tuple<int, int> size) const {
+Eigen::ArrayXXd Rng::Uniform(double min, double max, const std::tuple<int, int> size) {
+    swapIfMaxLessThanMin(min, max);
     std::uniform_real_distribution<double> dist(min, max);
     Eigen::ArrayXXd result =
-        Eigen::ArrayXXd::Zero(std::get<0>(size), std::get<1>(size)).unaryExpr([&](double) { return dist(mt); });
+        Eigen::ArrayXXd::Zero(std::get<0>(size), std::get<1>(size)).unaryExpr([&](double) { return dist(_mt); });
     return result;
 }
 
-double Rng::Random() const { return Uniform(0.0, 1.0); }
-Eigen::ArrayXd Rng::Random(const int size) const { return Uniform(0.0, 1.0, size); }
-Eigen::ArrayXXd Rng::Random(const std::tuple<int, int> size) const { return Uniform(0.0, 1.0, size); }
+double Rng::Random() { return Uniform(0.0, 1.0); }
+Eigen::ArrayXd Rng::Random(const int size) { return Uniform(0.0, 1.0, size); }
+Eigen::ArrayXXd Rng::Random(const std::tuple<int, int> size) { return Uniform(0.0, 1.0, size); }
 
 }  // namespace Eacpp
