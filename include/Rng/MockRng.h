8a588725e0fe4b1c183049443b174abc9bfc1f8d
hiro99988 <hrk9230.h@gmail.com>
2024-07-06 18:55:27 +0900

    RngのMockを作った

---
+++ include/Rng/MockRng.h
@@ -0,0 +1,34 @@
+#ifndef MockRng_H
+#define MockRng_H
+
+#include <gmock/gmock.h>
+
+#include <eigen3/Eigen/Core>
+#include <tuple>
+#include <vector>
+
+#include "Rng/IRng.h"
+
+namespace Eacpp {
+
+class MockRng : public IRng {
+   public:
+    MOCK_METHOD1(Integer, int(const int));
+    MOCK_METHOD2(Integer, int(int, int));
+    MOCK_METHOD4(Integers, std::vector<int>(int, int, const int, bool));
+
+    MOCK_METHOD2(Uniform, double(double, double));
+    MOCK_METHOD3(Uniform, Eigen::ArrayXd(double, double, const int));
+    MOCK_METHOD3(Uniform, Eigen::ArrayXXd(double, double, const std::tuple<int, int>));
+
+    MOCK_METHOD0(Random, double());
+    MOCK_METHOD1(Random, Eigen::ArrayXd(const int));
+    MOCK_METHOD1(Random, Eigen::ArrayXXd(const std::tuple<int, int>));
+
+    MOCK_METHOD3(Choice, Eigen::ArrayXXi(Eigen::ArrayXXi, int, bool));
+    MOCK_METHOD3(Choice, Eigen::ArrayXXd(Eigen::ArrayXXd, int, bool));
+};
+
+}  // namespace Eacpp
+
+#endif
