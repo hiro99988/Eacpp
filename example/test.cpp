7903a5565ce231efe07a71e1f9c21503edb7c833
hiro99988 <hrk9230.h@gmail.com>
2024-07-06 17:39:11 +0900

    一旦コードを全部消した

--- example/test.cpp
+++ example/test.cpp
@@ -5,35 +5,4 @@
 #include <random>
 #include <vector>
 
-// #include "Crossovers/OnePointCrossover.h"
-// #include
-"Rng/Rng.h"
-
-    using namespace std;
-// using namespace Eacpp;
-
-template <typename T>
-class OnePointCrossover {
-   public:
-    OnePointCrossover() {}
-    std::vector<Eigen::VectorX<T>> Cross(std::vector<Eigen::VectorX<T>> parents) {
-        throw std::runtime_error("Not implemented");
-    }
-};
-
-void swapIfMaxLessThanMin(int& min, int& max) {
-    if (max < min) {
-        std::swap(min, max);
-    }
-}
-
-Eigen::MatrixXd Mutate() { return Eigen::MatrixXd::Random(10, 10); }
-
-int main() {
-    int min = 100;
-    int max = 10;
-    swapIfMaxLessThanMin(min, max);
-    cout << min << " " << max << endl;
-
-    return 0;
-}
+int main() { return 0; }
