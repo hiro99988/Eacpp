a3c49bdee2feabc30dd8d0452e3850cce93abce2
hiro99988 <hrk9230.h@gmail.com>
2024-07-07 12:30:19 +0900

    BitFlipMutationをテスト

--- test/Mutations/TestBitFlipMutation.cpp
+++ test/Mutations/TestBitFlipMutation.cpp
@@ -2,15 +2,26 @@
 #include <gtest/gtest.h>
 
 // #include "Mutations/BitFlipMutation.h"
+#include "Mutations/BitFlipMutation.h"
 #include "Rng/MockRng.h"
 
 using ::testing::Return;
 
 TEST(BitFlipMutationTest, Mutate) {
     Eacpp::MockRng rng;
+    Eacpp::BitFlipMutation mutation(0.5, &rng);
+    Eigen::ArrayXi individual = Eigen::ArrayXi::Zero(10);
+    Eigen::ArrayXi expected(10);
+    expected << 1, 1, 1, 1, 1, 0, 0, 0, 0, 0;
 
-    EXPECT_CALL(rng, Random()).WillRepeatedly(Return(1.0));
-    double actual = rng.Random();
+    for (size_t i = 0; i < 5; ++i) {
+        EXPECT_CALL(rng, Random()).WillOnce(Return(0.9)).RetiresOnSaturation();
+    }
+    for (size_t i = 0; i < 5; ++i) {
+        EXPECT_CALL(rng, Random()).WillOnce(Return(0.1)).RetiresOnSaturation();
+    }
 
-    ASSERT_DOUBLE_EQ(1.0, actual);
+    mutation.Mutate(individual);
+
+    ASSERT_TRUE((individual == expected).all());
 }
