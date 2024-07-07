93729fedb31012813da35689708042a1d727fc11
hiro99988 <hrk9230.h@gmail.com>
2024-07-07 12:30:05 +0900

    IRngを持つように

--- include/Mutations/BitFlipMutation.h
+++ include/Mutations/BitFlipMutation.h
@@ -1,6 +1,9 @@
 #ifndef BitFlipMutation_h
 #define BitFlipMutation_h
 
+#include <eigen3/Eigen/Core>
+#include <iostream>
+
 #include "Mutations/IMutation.h"
 #include "Rng/IRng.h"
 #include "Rng/Rng.h"
@@ -10,13 +13,22 @@ class BitFlipMutation : public IMutation<int> {
    public:
     double mutationRate;
 
-    explicit BitFlipMutation(double mutationRate) : mutationRate(mutationRate), _rng(Rng()) {}
-    BitFlipMutation(double mutationRate, IRng rng) : mutationRate(mutationRate), _rng(rng) {}
+    explicit BitFlipMutation(double mutationRate) : mutationRate(mutationRate) {
+        _rng = new Rng();
+        _isRngCreated = true;
+    }
+    BitFlipMutation(double mutationRate, IRng* rng) : mutationRate(mutationRate), _rng(rng) {}
+    ~BitFlipMutation() {
+        if (_isRngCreated) {
+            delete _rng;
+        }
+    }
 
     void Mutate(Eigen::ArrayXi& individual) const override;
 
    private:
-    IRng& _rng;
+    IRng* _rng;
+    bool _isRngCreated = false;
 };
 
 }  // namespace Eacpp
--- src/Mutations/BitFlipMutation.cpp
+++ src/Mutations/BitFlipMutation.cpp
@@ -5,7 +5,7 @@
 namespace Eacpp {
 void BitFlipMutation::Mutate(Eigen::ArrayXi& individual) const {
     for (int i = 0; i < individual.size(); i++) {
-        if (_rng.Random() < mutationRate) {
+        if (_rng->Random() < mutationRate) {
             individual(i) = 1 - individual(i);
         }
     }
