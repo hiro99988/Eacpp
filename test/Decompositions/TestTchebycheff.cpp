ae0124b82ca0f4c22d632d2a409472af3ee3cd5c
hiro99988 <hrk9230.h@gmail.com>
2024-07-07 11:44:37 +0900

    Tchebycheffのテスト

---
+++ test/Decompositions/TestTchebycheff.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+#include <eigen3/Eigen/Core>
+
+#include "Decompositions/Tchebycheff.h"
+
+namespace Eacpp::Test {
+
+TEST(TchebycheffTest, ComputeObjective) {
+    Eacpp::Tchebycheff tchebycheff;
+    Eigen::ArrayXd weight(3);
+    Eigen::ArrayXd objectiveSet(3);
+    Eigen::ArrayXd referencePoint(3);
+    weight << 0.5, 0.4, 0.1;
+    objectiveSet << 2, 4, 6;
+    referencePoint << 1, 2, 3;
+    double actual = tchebycheff.ComputeObjective(weight, objectiveSet, referencePoint);
+    double expected = 0.8;
+    ASSERT_DOUBLE_EQ(actual, expected);
+}
+
+}  // namespace Eacpp::Test
