df5c2d71cac4c4af140c79a8fb016a0977f436ba
hiro99988 <hrk9230.h@gmail.com>
2024-07-07 11:43:18 +0900

    計算式が間違っていたので修正

--- src/Decompositions/Tchebycheff.cpp
+++ src/Decompositions/Tchebycheff.cpp
@@ -6,8 +6,8 @@ namespace Eacpp {
 
 double Tchebycheff::ComputeObjective(const Eigen::ArrayXd& weight, const Eigen::ArrayXd& objectiveSet,
                                      const Eigen::ArrayXd& referencePoint) const {
-    Eigen::ArrayXd diff = objectiveSet - referencePoint;
-    Eigen::ArrayXd weightedDiff = diff.cwiseProduct(weight);
+    Eigen::ArrayXd absDiff = (objectiveSet - referencePoint).abs();
+    Eigen::ArrayXd weightedDiff = weight * absDiff;
     return weightedDiff.maxCoeff();
 }
 
