d84d16bbc331cc9ebf7581cf77348dbb66c8b2e0
hiro99988 <hrk9230.h@gmail.com>
2024-07-30 05:56:28 +0900

    一通りmoeadが動くように（testなし)

--- example/MoeadEx.cpp
+++ example/MoeadEx.cpp
@@ -1,4 +1,6 @@
+#include <fstream>
 #include <iostream>
+#include <memory>
 
 #include "Algorithms/Moead.h"
 #include "Crossovers/OnePointCrossover.h"
@@ -11,21 +13,31 @@
 using namespace Eacpp;
 
 int main() {
-    int generationNum = 100;
-    int populationSize = 300;
+    int generationNum = 500;
     int decisionVariableNum = 10;
     int neighborNum = 20;
     int H = 299;
-    LZ1 problem(decisionVariableNum);
-    int objectiveNum = problem.objectiveNum;
-    OnePointCrossover<double> crossover(1.0);
-    Tchebycheff decomposition;
-    PolynomialMutation mutation(0.1, 20.0, problem.variableBounds);
-    UniformRandomSampling sampling(problem.variableBounds[0][0], problem.variableBounds[1][0]);
-    RandomSelection<double> selection;
+    std::shared_ptr<LZ1> problem = std::make_shared<LZ1>(decisionVariableNum);
+    int objectiveNum = problem->objectiveNum;
+    std::shared_ptr<OnePointCrossover<double>> crossover = std::make_shared<OnePointCrossover<double>>(1.0);
+    std::shared_ptr<Tchebycheff> decomposition = std::make_shared<Tchebycheff>();
+    std::shared_ptr<PolynomialMutation> mutation = std::make_shared<PolynomialMutation>(0.1, 20.0, problem->variableBounds);
+    std::shared_ptr<UniformRandomSampling> sampling =
+        std::make_shared<UniformRandomSampling>(problem->variableBounds[0][0], problem->variableBounds[1][0]);
+    std::shared_ptr<RandomSelection> selection = std::make_shared<RandomSelection>();
 
-    Moead<double> moead(generationNum, populationSize, decisionVariableNum, objectiveNum, neighborNum, H, &crossover,
-                        &decomposition, &mutation, &problem, &sampling, &selection);
+    Moead<double> moead(generationNum, decisionVariableNum, objectiveNum, neighborNum, H, crossover, decomposition, mutation,
+                        problem, sampling, selection);
+    moead.Run();
+
+    std::ofstream ofs("out/data/result.txt");
+    // Write the objective sets to the output file
+    for (const auto& set : moead.objectiveSets) {
+        for (const auto& value : set) {
+            ofs << value << " ";
+        }
+        ofs << std::endl;
+    }
 
     return 0;
 }
--- include/Algorithms/Moead.h
+++ include/Algorithms/Moead.h
@@ -2,8 +2,11 @@
 #define Moead_H
 
 #include <algorithm>
-#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
 #include <memory>
+#include <numeric>
+#include <ranges>
+#include <tuple>
 #include <vector>
 
 #include "Crossovers/ICrossover.h"
@@ -31,20 +34,18 @@ class Moead {
     std::shared_ptr<IMutation<DecisionVariableType>> mutation;
     std::shared_ptr<IProblem<DecisionVariableType>> problem;
     std::shared_ptr<ISampling<DecisionVariableType>> sampling;
-    std::shared_ptr<ISelection<DecisionVariableType>> selection;
-
-    Eigen::ArrayXXd weightVectors;
-    Eigen::ArrayXX<DecisionVariableType> solutions;
-    Eigen::ArrayXXd objectiveSets;
+    std::shared_ptr<ISelection> selection;
+    std::vector<Eigen::ArrayXd> weightVectors;
+    std::vector<Eigen::ArrayX<DecisionVariableType>> solutions;
+    std::vector<Eigen::ArrayXd> objectiveSets;
     Eigen::ArrayXd idealPoint;
-    Eigen::ArrayXXi neighborhoodIndexes;
+    std::vector<std::vector<int>> neighborhoodIndexes;
 
-    Moead(int generationNum, int populationSize, int decisionVariableNum, int objectiveNum, int neighborNum, int H,
-          ICrossover<DecisionVariableType>* crossover, IDecomposition* decomposition, IMutation<DecisionVariableType>* mutation,
-          IProblem<DecisionVariableType>* problem, ISampling<DecisionVariableType>* sampling,
-          ISelection<DecisionVariableType>* selection)
+    Moead(int generationNum, int decisionVariableNum, int objectiveNum, int neighborNum, int H,
+          std::shared_ptr<ICrossover<DecisionVariableType>> crossover, std::shared_ptr<IDecomposition> decomposition,
+          std::shared_ptr<IMutation<DecisionVariableType>> mutation, std::shared_ptr<IProblem<DecisionVariableType>> problem,
+          std::shared_ptr<ISampling<DecisionVariableType>> sampling, std::shared_ptr<ISelection> selection)
         : generationNum(generationNum),
-          populationSize(populationSize),
           decisionVariableNum(decisionVariableNum),
           objectiveNum(objectiveNum),
           neighborNum(neighborNum),
@@ -58,6 +59,7 @@ class Moead {
     virtual ~Moead() {}
 
     void Initialize() {
+        CaluculatePopulationNum();
         GenerateWeightVectors();
         GenerateNeighborhoods();
         InitializePopulation();
@@ -68,7 +70,7 @@ class Moead {
         for (int i = 0; i < populationSize; i++) {
             Eigen::ArrayX<DecisionVariableType> newSolution = GenerateNewSolution(i);
             if (!problem->IsFeasible(newSolution)) {
-                newSolution = sampling->Sample(1, decisionVariableNum);
+                newSolution = sampling->Sample(1, decisionVariableNum)[0];
             }
             Eigen::ArrayXd newObjectiveSet = problem->ComputeObjectiveSet(newSolution);
             UpdateIdealPoint(newObjectiveSet);
@@ -84,54 +86,63 @@ class Moead {
     }
 
    private:
+    void CaluculatePopulationNum() {
+        int n = H + objectiveNum - 1;
+        int r = objectiveNum - 1;
+        populationSize = Combination(n, r);
+    }
+
     void GenerateWeightVectors() {
         std::vector<double> takedSetTop(H + 1);
         std::iota(takedSetTop.begin(), takedSetTop.end(), 0);
-        auto product = Product(takedSetTop, objectiveNum);
-        product.erase(std::remove_if(product.begin(), product.end(),
-                                     [&](std::vector<int> v) { return std::reduce(v.begin(), v.end()) != H; }),
-                      product.end());
-
-        //     Eigen::ArrayXXd product = Eigen::ArrayXXd::Zero(std::pow(H + 1, decisionVariableNum), decisionVariableNum);
-        // for (int i = 0; i < decisionVariableNum; i++) {
-        //     product.col(i) = takedSetTop;
-        // }
-        // Eigen::ArrayXXd wvTop = product.rowwise().sum();
-        // weightVectors = wvTop.rowwise() / H;
+        std::vector<std::vector<double>> product = Product(takedSetTop, objectiveNum);
+        product.erase(
+            std::remove_if(product.begin(), product.end(), [&](auto v) { return std::reduce(v.begin(), v.end()) != H; }),
+            product.end());
+        for (auto&& v : product) {
+            weightVectors.push_back(Eigen::Map<Eigen::ArrayXd>(v.data(), v.size()));
+        }
     }
 
-    // def generate_weight_vector(m, H):
-    //     taked_set_top = [i for i in range(H + 1)]
-    //     product = np.array(list(itertools.product(taked_set_top, repeat=m)))
-    //     wv_top = product[np.sum(product, axis=1) == H]
-    //     wv = wv_top / H
-    //     return wv
-
     void GenerateNeighborhoods() {
-        Eigen::ArrayXd euclideanDistances(populationSize, populationSize);
+        std::vector<std::vector<std::pair<double, int>>> euclideanDistances(
+            populationSize, std::vector<std::pair<double, int>>(populationSize));
         for (int i = 0; i < populationSize; i++) {
-            euclideanDistances.col(i) = (weightVectors.colwise() - weightVectors.col(i)).colwise().squaredNorm();
+            for (int j = 0; j < populationSize; j++) {
+                euclideanDistances[i][j] = std::make_pair((weightVectors[i] - weightVectors[j]).matrix().squaredNorm(), j);
+            }
         }
-        neighborhoodIndexes.resize(neighborNum, populationSize);
+        neighborhoodIndexes = std::vector<std::vector<int>>(populationSize, std::vector<int>(neighborNum));
         for (int i = 0; i < populationSize; i++) {
-            std::sort(euclideanDistances.col(i).begin(), euclideanDistances.col(i).end());
-            neighborhoodIndexes.col(i) = euclideanDistances.col(i).head(neighborNum);
+            std::sort(euclideanDistances[i].begin(), euclideanDistances[i].end());
+            for (int j = 0; j < neighborNum; j++) {
+                neighborhoodIndexes[i][j] = euclideanDistances[i][j].second;
+            }
         }
     }
 
     void InitializePopulation() {
         solutions = sampling->Sample(populationSize, decisionVariableNum);
-        objectiveSets.resize(objectiveNum, populationSize);
-        for (int i = 0; i < populationSize; i++) {
-            objectiveSets.col(i) = problem->ComputeObjectiveSet(solutions.row(i));
+        objectiveSets.reserve(populationSize);
+        for (const auto& solution : solutions) {
+            objectiveSets.push_back(problem->ComputeObjectiveSet(solution));
         }
     }
 
-    void InitializeIdealPoint() { idealPoint = objectiveSets.rowwise().minCoeff(); }
+    void InitializeIdealPoint() {
+        Eigen::ArrayXd tmp = objectiveSets[0];
+        for (int i = 1; i < populationSize; i++) {
+            tmp = tmp.min(objectiveSets[i]);
+        }
+        idealPoint = tmp;
+    }
 
     Eigen::ArrayX<DecisionVariableType> GenerateNewSolution(int index) {
-        Eigen::ArrayXi childrenIndex = selection->Select(crossover->GetParentNum(), neighborhoodIndexes.col(index));
-        Eigen::ArrayXX<DecisionVariableType> parents = solutions.col(childrenIndex);
+        std::vector<int> childrenIndex = selection->Select(crossover->GetParentNum(), neighborhoodIndexes[index]);
+        std::vector<Eigen::ArrayX<DecisionVariableType>> parents;
+        for (const auto& i : childrenIndex) {
+            parents.push_back(solutions[i]);
+        }
         Eigen::ArrayX<DecisionVariableType> newSolution = crossover->Cross(parents);
         mutation->Mutate(newSolution);
         return newSolution;
@@ -140,12 +151,12 @@ class Moead {
     void UpdateIdealPoint(Eigen::ArrayXd objectiveSet) { idealPoint = idealPoint.min(objectiveSet); }
 
     void UpdateNeighboringSolutions(int index, Eigen::ArrayX<DecisionVariableType> solution, Eigen::ArrayXd objectiveSet) {
-        for (auto&& i : neighborhoodIndexes.col(index)) {
-            double newSubObjective = decomposition->ComputeObjective(weightVectors.col(i), objectiveSet, idealPoint);
-            double oldSubObjective = decomposition->ComputeObjective(weightVectors.col(i), objectiveSets.col(i), idealPoint);
+        for (auto&& i : neighborhoodIndexes[index]) {
+            double newSubObjective = decomposition->ComputeObjective(weightVectors[i], objectiveSet, idealPoint);
+            double oldSubObjective = decomposition->ComputeObjective(weightVectors[i], objectiveSets[i], idealPoint);
             if (newSubObjective <= oldSubObjective) {
-                solutions.col(i) = solution;
-                objectiveSets.col(i) = objectiveSet;
+                solutions[i] = solution;
+                objectiveSets[i] = objectiveSet;
             }
         }
     }
