f9d6dc0c52d13c57f05e0930e03f8dab32ac1002
hiro99988 <hrk9230.h@gmail.com>
2024-07-29 09:06:02 +0900

    組み合わせの関数

--- include/Utils/Utils.h
+++ include/Utils/Utils.h
@@ -56,6 +56,20 @@ std::vector<std::vector<T>> Product(const std::vector<T> &choices, const int rep
     return Utils::ProductRecurse(choices, repeat, std::vector<std::vector<T>>(1, std::vector<T>()));
 }
 
+inline long long Combination(int n, int r) {
+    std::vector<std::vector<long long>> v(n + 1, std::vector<long long>(n + 1, 0));
+    for (int i = 0; i < v.size(); i++) {
+        v[i][0] = 1;
+        v[i][i] = 1;
+    }
+    for (int j = 1; j < v.size(); j++) {
+        for (int k = 1; k < j; k++) {
+            v[j][k] = (v[j - 1][k - 1] + v[j - 1][k]);
+        }
+    }
+    return v[n][r];
+}
+
 }  // namespace Eacpp
 
 #endif
