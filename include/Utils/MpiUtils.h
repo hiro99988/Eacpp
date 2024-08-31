5541155e45a298eecfa53b13ea3bc619b12b0c57
hiro99988 <hrk9230.h@gmail.com>
2024-08-31 13:46:51 +0900

    変数の型に対応したMPIの型を取得する関数追加

--- include/Utils/MpiUtils.h
+++ include/Utils/MpiUtils.h
@@ -20,4 +20,49 @@ class MpiEnvironment : public ::testing::Environment {
     }
 };
 
+template <typename T>
+MPI_Datatype GetMpiDataType() {
+    if constexpr (std::is_same_v<signed char, T>) {
+        return MPI_CHAR;
+    } else if constexpr (std::is_same_v<signed short int, T>) {
+        return MPI_SHORT;
+    } else if constexpr (std::is_same_v<signed int, T>) {
+        return MPI_INT;
+    } else if constexpr (std::is_same_v<signed long int, T>) {
+        return MPI_LONG;
+    } else if constexpr (std::is_same_v<unsigned char, T>) {
+        return MPI_UNSIGNED_CHAR;
+    } else if constexpr (std::is_same_v<unsigned short int, T>) {
+        return MPI_UNSIGNED_SHORT;
+    } else if constexpr (std::is_same_v<unsigned int, T>) {
+        return MPI_UNSIGNED;
+    } else if constexpr (std::is_same_v<unsigned long int, T>) {
+        return MPI_UNSIGNED_LONG;
+    } else if constexpr (std::is_same_v<float, T>) {
+        return MPI_FLOAT;
+    } else if constexpr (std::is_same_v<double, T>) {
+        return MPI_DOUBLE;
+    } else if constexpr (std::is_same_v<long double, T>) {
+        return MPI_LONG_DOUBLE;
+    } else if constexpr (std::is_same_v<long long int, T>) {
+        return MPI_LONG_LONG_INT;
+    } else if constexpr (std::is_same_v<unsigned long long int, T>) {
+        return MPI_UNSIGNED_LONG_LONG;
+    } else if constexpr (std::is_same_v<std::pair<float, int>, T>) {
+        return MPI_FLOAT_INT;
+    } else if constexpr (std::is_same_v<std::pair<double, int>, T>) {
+        return MPI_DOUBLE_INT;
+    } else if constexpr (std::is_same_v<std::pair<long, int>, T>) {
+        return MPI_LONG_INT;
+    } else if constexpr (std::is_same_v<std::pair<int, int>, T>) {
+        return MPI_2INT;
+    } else if constexpr (std::is_same_v<std::pair<short, int>, T>) {
+        return MPI_SHORT_INT;
+    } else if constexpr (std::is_same_v<std::pair<long double, int>, T>) {
+        return MPI_LONG_DOUBLE_INT;
+    } else {
+        static_assert(false, "Unsupported type");
+    }
+}
+
 }  // namespace Eacpp
