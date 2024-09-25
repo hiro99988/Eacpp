b3726fc9c942824879be1a8890478d73438a54d5
hiro99988 <hrk9230.h@gmail.com>
2024-09-25 13:56:37 +0900

    変数名変更

--- include/Crossovers/CrossoverBase.h
+++ include/Crossovers/CrossoverBase.h
@@ -17,18 +17,18 @@ class CrossoverBase : public ICrossover<T> {
    public:
     double crossoverRate;
 
-    CrossoverBase(int parentNum, double crossoverRate) : _parentNum(parentNum), crossoverRate(crossoverRate) {
+    CrossoverBase(int parentNum, double crossoverRate) : _parentsNum(parentNum), crossoverRate(crossoverRate) {
         _rng = std::make_shared<Rng>();
     }
     CrossoverBase(int parentNum, double crossoverRate, std::shared_ptr<IRng> rng)
-        : _parentNum(parentNum), crossoverRate(crossoverRate), _rng(rng) {}
+        : _parentsNum(parentNum), crossoverRate(crossoverRate), _rng(rng) {}
     virtual ~CrossoverBase() {}
 
-    int GetParentNum() const override { return _parentNum; }
+    int GetParentNum() const override { return _parentsNum; }
 
     Individual<T> Cross(const std::vector<Individual<T>>& parents) const override {
         int actualParentNum = parents.size();
-        if (actualParentNum != _parentNum) {
+        if (actualParentNum != _parentsNum) {
             throw std::invalid_argument("Invalid size of parents");
         }
         return performCrossover(parents);
@@ -40,7 +40,7 @@ class CrossoverBase : public ICrossover<T> {
     virtual Individual<T> performCrossover(const std::vector<Individual<T>>& parents) const = 0;
 
    private:
-    int _parentNum;
+    int _parentsNum;
 };
 
 }  // namespace Eacpp
