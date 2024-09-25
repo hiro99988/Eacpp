016c5bf14cdf98f6b02e0226e972bf09d973a8aa
hiro99988 <hrk9230.h@gmail.com>
2024-09-25 13:07:47 +0900

    Individualに対応

--- src/Crossovers/BinomialCrossover.cpp
+++ src/Crossovers/BinomialCrossover.cpp
@@ -3,14 +3,16 @@
 #include <eigen3/Eigen/Core>
 #include <vector>
 
+#include "Individual/Individual.h"
+
 namespace Eacpp {
 
-Eigen::ArrayXd BinomialCrossover::performCrossover(const std::vector<Eigen::ArrayXd>& parents) const {
-    Eigen::ArrayXd child = parents[0];
-    int jr = _rng->Integer(child.size() - 1);
-    for (int i = 0; i < child.size(); i++) {
+Individuald BinomialCrossover::performCrossover(const std::vector<Individuald>& parents) const {
+    Individuald child(parents[0].solution);
+    int jr = _rng->Integer(child.solution.size() - 1);
+    for (int i = 0; i < child.solution.size(); i++) {
         if (_rng->Random() <= crossoverRate || i == jr) {
-            child(i) += scalingFactor * (parents[1](i) - parents[2](i));
+            child.solution(i) += scalingFactor * (parents[1].solution(i) - parents[2].solution(i));
         }
     }
     return child;
--- test/Crossovers/BinomialCrossoverTest.cpp
+++ test/Crossovers/BinomialCrossoverTest.cpp
@@ -5,6 +5,7 @@
 #include <memory>
 
 #include "Crossovers/BinomialCrossover.h"
+#include "Individual/Individual.h"
 #include "Rng/MockRng.h"
 
 using ::testing::_;
@@ -20,18 +21,15 @@ TEST(BinomialCrossoverTest, PerformCrossover) {
     EXPECT_CALL(*mockRng, Integer(_)).Times(1).WillOnce(Return(1));
     EXPECT_CALL(*mockRng, Random()).WillRepeatedly(Return(0.0));
 
-    Eigen::ArrayXd parent1(5);
-    parent1 << 1, 2, 3, 4, 5;
-    Eigen::ArrayXd parent2(5);
-    parent2 << 6, 7, 8, 9, 10;
-    Eigen::ArrayXd parent3(5);
-    parent3 << 11, 12, 13, 14, 15;
-    std::vector parents = {parent1, parent2, parent3};
-    Eigen::ArrayXd expected(5);
-    expected << -1.5, -0.5, 0.5, 1.5, 2.5;
+    Individuald parent1(Eigen::ArrayXd::LinSpaced(5, 1, 5));
+    Individuald parent2(Eigen::ArrayXd::LinSpaced(5, 6, 10));
+    Individuald parent3(Eigen::ArrayXd::LinSpaced(5, 11, 15));
+    std::vector<Individuald> parents = {parent1, parent2, parent3};
+    Individuald expected(5);
+    expected.solution << -1.5, -0.5, 0.5, 1.5, 2.5;
 
-    Eigen::ArrayXd actual = binomialCrossover.Cross(parents);
-    ASSERT_TRUE((expected == actual).all());
+    auto actual = binomialCrossover.Cross(parents);
+    ASSERT_TRUE(actual == expected);
 }
 
 }  // namespace Eacpp::Test
