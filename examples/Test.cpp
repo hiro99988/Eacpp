779dc62e32841b9cf25fdcedd942201ccd0882fd
hiro99988 <hrk9230.h@gmail.com>
2024-09-30 08:04:45 +0900

    テストファイル

---
+++ examples/Test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "Individual/Individual.h"
+
+using namespace Eacpp;
+
+int main() {
+    Individuali individual(Eigen::ArrayXi::LinSpaced(3, 0, 2), Eigen::ArrayXd::Constant(2, 1.0),
+                           Eigen::ArrayXd::Constant(2, 0.5), {1, 2});
+    Individuali other;
+    other = individual;
+    std::cout << individual << std::endl;
+    std::cout << other << std::endl;
+
+    return 0;
+}
