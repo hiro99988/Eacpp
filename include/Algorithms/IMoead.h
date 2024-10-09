aef6936a357dd389c8d5661e9de88bfaa52dd330
hiro99988 <hrk9230.h@gmail.com>
2024-10-09 14:20:37 +0900

    Moeadのインターフェース

---
+++ include/Algorithms/IMoead.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <eigen3/Eigen/Core>
+
+namespace Eacpp {
+
+template <typename DecisionVariableType>
+struct IMoead {
+    virtual void Initialize() = 0;
+    virtual void Update() = 0;
+    virtual void Run() = 0;
+    virtual double GetExecutionTime() = 0;
+    virtual std::vector<Eigen::ArrayXd> GetObjectivesList() = 0;
+    virtual std::vector<Eigen::ArrayX<DecisionVariableType>> GetSolutionList() = 0;
+    virtual std::vector<Eigen::ArrayXd> GetTransitionOfIdealPoint() = 0;
+};
+
+}  // namespace Eacpp
