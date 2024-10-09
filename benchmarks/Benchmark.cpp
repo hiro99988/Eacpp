8e98a5ff712810f98109f5cc698eed1e44f0ce02
hiro99988 <hrk9230.h@gmail.com>
2024-10-09 14:24:04 +0900

    ベンチマーク用ファイル作成

---
+++ benchmarks/Benchmark.cpp
@@ -0,0 +1,14 @@
+#include <mpi.h>
+
+#include <eigen3/Eigen/Core>
+#include <filesystem>
+#include <iostream>
+#include <memory>
+
+#include "Algorithms/IMoead.h"
+#include "Algorithms/Moead.h"
+#include "Problems/Problems.h"
+
+int main(int argc, char** argv) {
+    return 0;
+}
