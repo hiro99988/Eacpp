e1748ee7037a207aa127fd45f109462d703c6fad
hiro99988 <hrk9230.h@gmail.com>
2024-10-14 09:53:21 +0900

    必要な関数追加

--- include/Algorithms/IMoead.h
+++ include/Algorithms/IMoead.h
@@ -6,9 +6,12 @@ namespace Eacpp {
 
 template <typename DecisionVariableType>
 struct IMoead {
+    virtual ~IMoead() {}
+
     virtual void Initialize() = 0;
     virtual void Update() = 0;
     virtual void Run() = 0;
+    virtual bool IsEnd() const = 0;
     virtual std::vector<Eigen::ArrayXd> GetObjectivesList() const = 0;
     virtual std::vector<Eigen::ArrayX<DecisionVariableType>> GetSolutionList() const = 0;
 };
