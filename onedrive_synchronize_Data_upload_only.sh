9dd6df4db639fd9e91330e50817ee623530dc898
hiro99988 <hrk9230.h@gmail.com>
2024-10-23 15:06:40 +0900

    つかないため削除

--- onedrive_synchronize_Data_upload_only.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mv out/data/Moead/* ~/OneDrive/HaradaLab/Data/Moead/
-mv out/data/MpMoead/* ~/OneDrive/HaradaLab/Data/MpMoead/
-onedrive --synchronize --single-directory --upload-only "HaradaLab/Data/"
