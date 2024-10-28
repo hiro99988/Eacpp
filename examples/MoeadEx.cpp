2e2e40490555a229979e57d0237c9fb6fec59882
hiro99988 <hrk9230.h@gmail.com>
2024-10-28 12:41:54 +0900

    コマンドライン引数で問題をしていできるように

--- examples/MoeadEx.cpp
+++ examples/MoeadEx.cpp
@@ -5,14 +5,16 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <string>
 #include <vector>
 
 #include "Algorithms/Moead.h"
 #include "Crossovers/SimulatedBinaryCrossover.h"
 #include "Decompositions/Tchebycheff.h"
 #include "Mutations/PolynomialMutation.h"
-#include "Problems/ZDT4.h"
-#include "Repairs/SamplingRepair.h"
+#include "Problems/Problems.h"
+#include "Reflections/Reflection.h"
+#include "Repairs/RealRandomRepair.h"
 #include "Samplings/RealRandomSampling.h"
 #include "Selections/RandomSelection.h"
 
@@ -22,6 +24,7 @@ int main(int argc, char* argv[]) {
     int generationNum = 500;
     int H = 299;
     int neighborNum = 21;
+    std::string problemName = "ZDT1";
 
     if (argc == 2) {
         generationNum = std::stoi(argv[1]);
@@ -32,19 +35,21 @@ int main(int argc, char* argv[]) {
         generationNum = std::stoi(argv[1]);
         H = std::stoi(argv[2]);
         neighborNum = std::stoi(argv[3]);
-    } else if (argc >= 5) {
-        std::cerr << "Usage: " << argv[0] << " [generationNum] [H] [neighborNum]" << std::endl;
-        return 1;
+    } else if (argc == 5) {
+        generationNum = std::stoi(argv[1]);
+        H = std::stoi(argv[2]);
+        neighborNum = std::stoi(argv[3]);
+        problemName = argv[4];
     }
 
-    auto problem = std::make_shared<ZDT4>();
+    std::shared_ptr<IProblem<double>> problem = Reflection<IProblem<double>>::Create(problemName);
 
     auto crossover = std::make_shared<SimulatedBinaryCrossover>(0.9);
     auto decomposition = std::make_shared<Tchebycheff>();
     auto mutation =
         std::make_shared<PolynomialMutation>(1.0 / problem->DecisionVariablesNum(), 20.0, problem->VariableBounds());
     auto sampling = std::make_shared<RealRandomSampling>(problem->VariableBounds());
-    auto repair = std::make_shared<SamplingRepair<double>>(sampling);
+    auto repair = std::make_shared<RealRandomRepair>(problem);
     auto selection = std::make_shared<RandomSelection>();
 
     Moead<double> moead(generationNum, neighborNum, H, crossover, decomposition, mutation, problem, repair, sampling,
@@ -58,7 +63,7 @@ int main(int argc, char* argv[]) {
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
     std::cout << "Execution time: " << duration << "ms" << std::endl;
 
-    std::filesystem::path objectiveFilePath = "out/data/tmp/ZDT4.csv";
+    std::filesystem::path objectiveFilePath = "out/data/tmp/ZDT1.csv";
     std::ofstream objectiveFile(objectiveFilePath);
     for (const auto& objectives : moead.GetObjectivesList()) {
         for (size_t j = 0; j < objectives.size(); j++) {
