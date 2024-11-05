69b2f62a607838f64b183e557f8abb4396858037
hiro99988 <hrk9230.h@gmail.com>
2024-11-05 12:14:26 +0900

    NtMoeadのexampleを使いやすく

--- examples/NtMoeadEx.cpp
+++ examples/NtMoeadEx.cpp
@@ -19,18 +19,62 @@
 
 using namespace Eacpp;
 
-int main() {
-    auto problem = std::make_shared<ZDT1>();
+int main(int argc, char** argv) {
+    int rank;
+    MPI_Init(nullptr, nullptr);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int generationNum = 500;
+    int neighborhoodSize = 21;
+    int migrationInterval = 1;
+    int H = 299;
+    std::string problemName = "ZDT1";
+
+    if (argc == 6) {
+        generationNum = std::stoi(argv[1]);
+        neighborhoodSize = std::stoi(argv[2]);
+        migrationInterval = std::stoi(argv[3]);
+        H = std::stoi(argv[4]);
+        problemName = argv[5];
+    }
+
+    std::shared_ptr<IProblem<double>> problem = Reflection<IProblem<double>>::Create(problemName);
+
     auto crossover = std::make_shared<SimulatedBinaryCrossover>(0.9, problem->VariableBounds());
     auto decomposition = std::make_shared<Tchebycheff>();
     auto mutation = std::make_shared<PolynomialMutation>(1.0 / problem->DecisionVariablesNum(), problem->VariableBounds());
-    auto repair = std::make_shared<RealRandomRepair>(problem);
     auto sampling = std::make_shared<RealRandomSampling>(problem->VariableBounds());
+    auto repair = std::make_shared<RealRandomRepair>(problem);
     auto selection = std::make_shared<RandomSelection>();
 
-    auto ntMoead = NtMoead<double>(500, 21, 59, 1, crossover, decomposition, mutation, problem, repair, sampling, selection);
+    auto moead = NtMoead<double>(500, 21, 59, 1, crossover, decomposition, mutation, problem, repair, sampling, selection);
 
-    ntMoead.Initialize();
+    double start = MPI_Wtime();
+
+    moead.Initialize();
+    // moead.Run();
+
+    double end = MPI_Wtime();
+    double executionTime = end - start;
+    double maxTime;
+    MPI_Reduce(&executionTime, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        std::cout << "Maximum execution time across all processes: " << maxTime << " seconds" << std::endl;
+    }
+
+    // std::filesystem::path objectiveFilePath = "out/data/tmp/objective/" + std::to_string(rank) + ".csv";
+    // std::ofstream objectiveFile(objectiveFilePath);
+    // for (const auto& objectives : moead.GetObjectivesList()) {
+    //     for (int i = 0; i < objectives.size(); i++) {
+    //         objectiveFile << objectives[i];
+    //         if (i != objectives.size() - 1) {
+    //             objectiveFile << ",";
+    //         }
+    //     }
+    //     objectiveFile << std::endl;
+    // }
+
+    MPI_Finalize();
 
     return 0;
 }
