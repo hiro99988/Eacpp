800e1c242b8bd6baf8c865cf5ea4964141f3c7d8
hiro99988 <hrk9230.h@gmail.com>
2024-11-18 12:27:52 +0900

    ベンチマークを新しく

--- benchmarks/MoeadBenchmark.cpp
+++ benchmarks/MoeadBenchmark.cpp
@@ -1,6 +1,5 @@
 #include <mpi.h>
 
-#include <chrono>
 #include <eigen3/Eigen/Core>
 #include <filesystem>
 #include <fstream>
@@ -21,43 +20,11 @@
 #include "Repairs/RealRandomRepair.h"
 #include "Samplings/RealRandomSampling.h"
 #include "Selections/RandomSelection.h"
+#include "Stopwatches/Stopwatch.hpp"
+#include "Utils/FileUtils.h"
+#include "Utils/Utils.h"
 
 using namespace Eacpp;
-
-std::ifstream OpenInputFile(const std::filesystem::path& filePath) {
-    std::ifstream fileStream(filePath);
-    if (!fileStream) {
-        std::cerr << "Error: Could not open " << filePath << std::endl;
-        std::exit(1);
-    }
-    return fileStream;
-}
-
-std::ofstream OpenOutputFile(const std::filesystem::path& filePath) {
-    std::ofstream fileStream(filePath);
-    if (!fileStream) {
-        std::cerr << "Error: Could not create or open " << filePath << std::endl;
-        std::exit(1);
-    }
-    return fileStream;
-}
-
-void CreateDirectories(const std::filesystem::path& dirPath) {
-    try {
-        std::filesystem::create_directories(dirPath);
-    } catch (const std::filesystem::filesystem_error& e) {
-        std::cerr << "Error: Could not create directories " << dirPath << std::endl;
-        std::exit(1);
-    }
-}
-
-std::string GetTimestamp() {
-    auto now = std::chrono::system_clock::now();
-    auto nowTime = std::chrono::system_clock::to_time_t(now);
-    std::stringstream ss;
-    ss << std::put_time(std::localtime(&nowTime), "%y%m%d-%H%M%S");
-    return ss.str();
-}
 
 void AddIdealPoint(int gen, const Eigen::ArrayXd& add, std::vector<std::pair<int, Eigen::ArrayXd>>& vec) {
     if (vec.size() == 0) {
@@ -72,6 +39,7 @@ void AddIdealPoint(int gen, const Eigen::ArrayXd& add, std::vector<std::pair<int
 
 int main(int argc, char** argv) {
     constexpr const char* ParameterFilePath = "data/inputs/benchmarks/parameter.json";
+    constexpr static std::array<const char*, 2> ExecutionTimesHeader = {"trial", "time(s)"};
 
     auto parameterFile = OpenInputFile(ParameterFilePath);
 
@@ -85,17 +53,19 @@ int main(int argc, char** argv) {
     std::vector<std::string> problemNames = parameter["problems"];
 
     const std::filesystem::path outputDirectoryPath = "out/data/Moead/" + GetTimestamp() + "/";
-    CreateDirectories(outputDirectoryPath);
+    std::filesystem::create_directories(outputDirectoryPath);
 
+    Stopwatch stopwatch;
     for (auto&& problemName : problemNames) {
         const std::filesystem::path outputProblemDirectoryPath = outputDirectoryPath / problemName;
         const std::filesystem::path objectiveDirectoryPath = outputProblemDirectoryPath / "objective";
         const std::filesystem::path idealPointDirectoryPath = outputProblemDirectoryPath / "idealPoint";
-        CreateDirectories(outputProblemDirectoryPath);
-        CreateDirectories(objectiveDirectoryPath);
-        CreateDirectories(idealPointDirectoryPath);
+        std::filesystem::create_directories(outputProblemDirectoryPath);
+        std::filesystem::create_directories(objectiveDirectoryPath);
+        std::filesystem::create_directories(idealPointDirectoryPath);
         const std::filesystem::path executionTimesFilePath = outputProblemDirectoryPath / "executionTimes.csv";
         std::ofstream executionTimesFile = OpenOutputFile(executionTimesFilePath);
+        WriteCsvLine(executionTimesFile, ExecutionTimesHeader);
 
         std::shared_ptr<IProblem<double>> problem = std::move(Reflection<IProblem<double>>::Create(problemName));
         auto crossover = std::make_shared<SimulatedBinaryCrossover>(crossoverRate, problem->VariableBounds());
@@ -105,69 +75,50 @@ int main(int argc, char** argv) {
         auto repair = std::make_shared<RealRandomRepair>(problem);
         auto selection = std::make_shared<RandomSelection>();
 
+        std::vector<std::string> objectiveHeader;
+        for (int i = 0; i < problem->ObjectivesNum(); i++) {
+            objectiveHeader.push_back("objective" + std::to_string(i + 1));
+        }
+        std::vector<std::string> idealPointHeader = {"generation"};
+        for (int i = 0; i < problem->ObjectivesNum(); i++) {
+            idealPointHeader.push_back("objective" + std::to_string(i + 1));
+        }
+
         std::cout << "Problem: " << problemName << std::endl;
 
         for (int i = 0; i < trial; i++) {
             Moead<double> moead(generationNum, neighborhoodSize, divisionsNumOfWeightVector, crossover, decomposition, mutation,
                                 problem, repair, sampling, selection);
 
-            int generation = 0;
-            double totalExecutionTime = 0.0;
             std::vector<std::pair<int, Eigen::ArrayXd>> transitionOfIdealPoint;
 
-            auto start = std::chrono::high_resolution_clock::now();
+            stopwatch.Restart();
 
             moead.Initialize();
 
-            auto end = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> elapsed = end - start;
-            totalExecutionTime += elapsed.count();
-
-            AddIdealPoint(generation, decomposition->IdealPoint(), transitionOfIdealPoint);
+            stopwatch.Stop();
+            AddIdealPoint(moead.CurrentGeneration(), decomposition->IdealPoint(), transitionOfIdealPoint);
 
             while (!moead.IsEnd()) {
-                start = std::chrono::high_resolution_clock::now();
+                stopwatch.Start();
 
                 moead.Update();
 
-                end = std::chrono::high_resolution_clock::now();
-                elapsed = end - start;
-                totalExecutionTime += elapsed.count();
-
-                generation++;
-                AddIdealPoint(generation, decomposition->IdealPoint(), transitionOfIdealPoint);
+                stopwatch.Stop();
+                AddIdealPoint(moead.CurrentGeneration(), decomposition->IdealPoint(), transitionOfIdealPoint);
             }
 
-            std::cout << "Trial " << i + 1 << " Total execution time: " << totalExecutionTime << " seconds" << std::endl;
+            std::cout << "Trial " << i + 1 << " Total execution time: " << stopwatch.Elapsed() << " seconds" << std::endl;
 
-            std::filesystem::path objectiveFilePath = objectiveDirectoryPath / (std::to_string(i + 1) + ".csv");
+            std::filesystem::path objectiveFilePath = objectiveDirectoryPath / ("trial_" + std::to_string(i + 1) + ".csv");
             std::ofstream objectiveFile = OpenOutputFile(objectiveFilePath);
-            for (const auto& objectives : moead.GetObjectivesList()) {
-                for (size_t j = 0; j < objectives.size(); j++) {
-                    if (j == objectives.size() - 1) {
-                        objectiveFile << objectives[j];
-                    } else {
-                        objectiveFile << objectives[j] << ",";
-                    }
-                }
-                objectiveFile << std::endl;
-            }
+            WriteCsv(objectiveFile, moead.GetObjectivesList(), objectiveHeader);
 
-            std::filesystem::path idealPointFilePath = idealPointDirectoryPath / (std::to_string(i + 1) + ".csv");
+            std::filesystem::path idealPointFilePath = idealPointDirectoryPath / ("trial_" + std::to_string(i + 1) + ".csv");
             std::ofstream idealPointFile = OpenOutputFile(idealPointFilePath);
-            for (const auto& idealPoint : transitionOfIdealPoint) {
-                idealPointFile << idealPoint.first << ",";
-                for (size_t j = 0; j < idealPoint.second.size(); j++) {
-                    if (j == idealPoint.second.size() - 1) {
-                        idealPointFile << idealPoint.second(j);
-                    } else {
-                        idealPointFile << idealPoint.second(j) << ",";
-                    }
-                }
-                idealPointFile << std::endl;
-            }
+            WriteCsv(idealPointFile, transitionOfIdealPoint, idealPointHeader);
 
-            executionTimesFile << i + 1 << "," << totalExecutionTime << std::endl;
+            executionTimesFile << i + 1 << "," << stopwatch.Elapsed() << std::endl;
         }
     }
 
---
+++ benchmarks/ParallelMoeadBenchmark.cpp
@@ -0,0 +1,288 @@
+#include <mpi.h>
+
+#include <chrono>
+#include <eigen3/Eigen/Core>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "Algorithms/MpMoead.h"
+#include "Algorithms/NtMoead.h"
+#include "Crossovers/SimulatedBinaryCrossover.h"
+#include "Decompositions/Tchebycheff.h"
+#include "Mutations/PolynomialMutation.h"
+#include "Problems/Problems.h"
+#include "Reflections/Reflection.h"
+#include "Repairs/RealRandomRepair.h"
+#include "Samplings/RealRandomSampling.h"
+#include "Selections/RandomSelection.h"
+#include "Stopwatches/MpiStopwatch.hpp"
+#include "Utils/FileUtils.h"
+#include "Utils/MpiUtils.h"
+#include "Utils/Utils.h"
+
+namespace Eacpp {
+
+class ParallelMoeadBenchmark {
+   public:
+    constexpr static const char* DefaultParameterFilePath = "data/inputs/benchmarks/parameter.json";
+    constexpr static std::array<const char*, 2> MoeadNames = {"MpMoead", "NtMoead"};
+    constexpr static std::array<const char*, 2> ExecutionTimesHeader = {"trial", "time(s)"};
+
+    int rank;
+    int parallelSize;
+    std::string moeadName;
+    std::filesystem::path parameterFilePath;
+    std::vector<std::pair<int, Eigen::ArrayXd>> transitionOfIdealPoint;
+
+    ParallelMoeadBenchmark(const std::string& moeadName) : parameterFilePath(DefaultParameterFilePath) {
+        InitializeMoeadName(moeadName);
+    }
+    ParallelMoeadBenchmark(const std::string& moeadName, const std::filesystem::path& parameterFilePath)
+        : parameterFilePath(parameterFilePath) {
+        InitializeMoeadName(moeadName);
+    }
+
+    void InitializeMoeadName(const std::string& moeadName) {
+        if (std::ranges::find(MoeadNames, moeadName) == MoeadNames.end()) {
+            throw std::invalid_argument("Invalid moead name \"" + moeadName + "\"");
+        }
+
+        this->moeadName = moeadName;
+    }
+
+    void InitializeMpi() {
+        int initialized;
+        MPI_Initialized(&initialized);
+        if (!initialized) {
+            MPI_Init(nullptr, nullptr);
+        }
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &parallelSize);
+    }
+
+    void ReadParameters(std::ifstream& file, int& outTrial, int& outGenerationNum, int& outNeighborhoodSize,
+                        int& outDivisionsNumOfWeightVector, int& outMigrationInterval, double& outCrossoverRate,
+                        bool& outIdealPointMigration, std::vector<std::string>& outProblemNames) {
+        nlohmann::json parameter = nlohmann::json::parse(file);
+
+        outTrial = parameter["trial"];
+        outGenerationNum = parameter["generationNum"];
+        outNeighborhoodSize = parameter["neighborhoodSize"];
+        outDivisionsNumOfWeightVector = parameter["divisionsNumOfWeightVector"];
+        outMigrationInterval = parameter["migrationInterval"];
+        outCrossoverRate = parameter["crossoverRate"];
+        outIdealPointMigration = parameter["idealPointMigration"];
+        outProblemNames = parameter["problems"];
+    }
+
+    void AddIdealPoint(int gen, const Eigen::ArrayXd& add) {
+        if (transitionOfIdealPoint.size() == 0) {
+            transitionOfIdealPoint.push_back(std::make_pair(gen, add));
+            return;
+        }
+
+        if ((add != transitionOfIdealPoint.back().second).any()) {
+            transitionOfIdealPoint.push_back(std::make_pair(gen, add));
+        }
+    }
+
+    std::vector<std::pair<int, std::vector<double>>> GatherObjectivesList(
+        const std::vector<Eigen::ArrayXd>& localObjectivesList) {
+        std::vector<double> sendBuffer;
+        int dataSize = localObjectivesList[0].size();
+        sendBuffer.reserve(dataSize * localObjectivesList.size());
+        for (const auto& objectives : localObjectivesList) {
+            sendBuffer.insert(sendBuffer.end(), objectives.begin(), objectives.end());
+        }
+
+        std::vector<double> receiveBuffer;
+        std::vector<int> sizes;
+        Gatherv(sendBuffer, rank, parallelSize, receiveBuffer, sizes);
+
+        std::vector<std::pair<int, std::vector<double>>> objectivesList;
+        if (rank == 0) {
+            for (int i = 0, count = 0; i < parallelSize; count += sizes[i], i++) {
+                for (int j = 0; j < sizes[i]; j += dataSize) {
+                    std::vector<double> objectives(receiveBuffer.begin() + count + j,
+                                                   receiveBuffer.begin() + count + j + dataSize);
+                    objectivesList.push_back(std::make_pair(i, std::move(objectives)));
+                }
+            }
+        }
+
+        return objectivesList;
+    }
+
+    std::vector<std::pair<std::array<int, 2>, std::vector<double>>> GatherTransitionOfIdealPoint() {
+        std::vector<double> sendBuffer;
+        int dataSize = transitionOfIdealPoint[0].second.size() + 1;
+        sendBuffer.reserve(dataSize * transitionOfIdealPoint.size());
+        for (const auto& [gen, idealPoint] : transitionOfIdealPoint) {
+            sendBuffer.push_back(gen);
+            sendBuffer.insert(sendBuffer.end(), idealPoint.begin(), idealPoint.end());
+        }
+
+        std::vector<double> receiveBuffer;
+        std::vector<int> sizes;
+        Gatherv(sendBuffer, rank, parallelSize, receiveBuffer, sizes);
+
+        std::vector<std::pair<std::array<int, 2>, std::vector<double>>> transitionOfIdealPointList;
+        if (rank == 0) {
+            for (int i = 0, count = 0; i < parallelSize; count += sizes[i], i++) {
+                for (int j = 0; j < sizes[i]; j += dataSize) {
+                    std::array<int, 2> keys = {i, static_cast<int>(receiveBuffer[count + j])};
+                    std::vector<double> objectives(receiveBuffer.begin() + count + j + 1,
+                                                   receiveBuffer.begin() + count + j + dataSize);
+                    transitionOfIdealPointList.push_back(std::make_pair(std::move(keys), std::move(objectives)));
+                }
+            }
+        }
+
+        return transitionOfIdealPointList;
+    }
+
+    void Run() {
+        InitializeMpi();
+
+        int trial;
+        int generationNum;
+        int neighborhoodSize;
+        int divisionsNumOfWeightVector;
+        int migrationInterval;
+        double crossoverRate;
+        bool idealPointMigration;
+        std::vector<std::string> problemNames;
+        std::ifstream parameterFile(parameterFilePath);
+        ReadParameters(parameterFile, trial, generationNum, neighborhoodSize, divisionsNumOfWeightVector, migrationInterval,
+                       crossoverRate, idealPointMigration, problemNames);
+
+        const std::filesystem::path outputDirectoryPath = "out/data/" + moeadName + "/" + GetTimestamp() + "/";
+        RANK0(std::filesystem::create_directories(outputDirectoryPath);)
+
+        MpiStopwatch stopwatch;
+        for (auto&& problemName : problemNames) {
+            const std::filesystem::path outputProblemDirectoryPath = outputDirectoryPath / problemName;
+            const std::filesystem::path objectiveDirectoryPath = outputProblemDirectoryPath / "objective";
+            const std::filesystem::path idealPointDirectoryPath = outputProblemDirectoryPath / "idealPoint";
+            RANK0(std::filesystem::create_directories(outputProblemDirectoryPath);
+                  std::filesystem::create_directories(objectiveDirectoryPath);
+                  std::filesystem::create_directories(idealPointDirectoryPath);)
+
+            const std::filesystem::path executionTimesFilePath = outputProblemDirectoryPath / "executionTimes.csv";
+            std::ofstream executionTimesFile;
+            RANK0(executionTimesFile = OpenOutputFile(executionTimesFilePath);
+                  WriteCsvLine(executionTimesFile, ExecutionTimesHeader);)
+
+            std::shared_ptr<IProblem<double>> problem = std::move(Reflection<IProblem<double>>::Create(problemName));
+            auto crossover = std::make_shared<SimulatedBinaryCrossover>(crossoverRate, problem->VariableBounds());
+            auto decomposition = std::make_shared<Tchebycheff>();
+            auto mutation =
+                std::make_shared<PolynomialMutation>(1.0 / problem->DecisionVariablesNum(), problem->VariableBounds());
+            auto sampling = std::make_shared<RealRandomSampling>(problem->VariableBounds());
+            auto repair = std::make_shared<RealRandomRepair>(problem);
+            auto selection = std::make_shared<RandomSelection>();
+
+            std::vector<std::string> objectiveHeader = {"rank"};
+            for (int i = 0; i < problem->ObjectivesNum(); i++) {
+                objectiveHeader.push_back("objective" + std::to_string(i + 1));
+            }
+            std::vector<std::string> idealPointHeader = {"rank", "generation"};
+            for (int i = 0; i < problem->ObjectivesNum(); i++) {
+                idealPointHeader.push_back("objective" + std::to_string(i + 1));
+            }
+
+            RANK0(std::cout << "Problem: " << problemName << std::endl)
+
+            for (int i = 0; i < trial; i++) {
+                transitionOfIdealPoint.clear();
+                std::unique_ptr<IMoead<double>> moead;
+                if (moeadName == MoeadNames[0]) {
+                    moead = std::make_unique<MpMoead<double>>(generationNum, neighborhoodSize, divisionsNumOfWeightVector,
+                                                              migrationInterval, crossover, decomposition, mutation, problem,
+                                                              repair, sampling, selection, idealPointMigration);
+                } else if (moeadName == MoeadNames[1]) {
+                    moead = std::make_unique<NtMoead<double>>(generationNum, neighborhoodSize, divisionsNumOfWeightVector,
+                                                              migrationInterval, crossover, decomposition, mutation, problem,
+                                                              repair, sampling, selection, idealPointMigration);
+                } else {
+                    throw std::invalid_argument("Invalid moead name");
+                }
+
+                MPI_Barrier(MPI_COMM_WORLD);
+                stopwatch.Restart();
+
+                moead->Initialize();
+
+                stopwatch.Stop();
+                AddIdealPoint(moead->CurrentGeneration(), decomposition->IdealPoint());
+
+                while (!moead->IsEnd()) {
+                    stopwatch.Start();
+
+                    moead->Update();
+
+                    stopwatch.Stop();
+                    AddIdealPoint(moead->CurrentGeneration(), decomposition->IdealPoint());
+                }
+
+                double elapsed = stopwatch.Elapsed();
+                double maxExecutionTime;
+                MPI_Reduce(&elapsed, &maxExecutionTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+                RANK0(std::cout << "Trial " << i + 1 << " Total execution time: " << maxExecutionTime << " seconds"
+                                << std::endl;)
+                RANK0(executionTimesFile << i + 1 << "," << maxExecutionTime << std::endl;)
+
+                std::filesystem::path objectiveFilePath = objectiveDirectoryPath / ("trial_" + std::to_string(i + 1) + ".csv");
+                std::ofstream objectiveFile = OpenOutputFile(objectiveFilePath);
+                auto localObjectivesList = moead->GetObjectivesList();
+                auto objectivesList = GatherObjectivesList(localObjectivesList);
+                RANK0(WriteCsv(objectiveFile, objectivesList, objectiveHeader);)
+
+                std::filesystem::path idealPointFilePath =
+                    idealPointDirectoryPath / ("trial_" + std::to_string(i + 1) + ".csv");
+                std::ofstream idealPointFile = OpenOutputFile(idealPointFilePath);
+                auto transitionOfIdealPointList = GatherTransitionOfIdealPoint();
+                RANK0(WriteCsv(idealPointFile, transitionOfIdealPointList, idealPointHeader);)
+
+                MPI_Barrier(MPI_COMM_WORLD);
+                ReleaseIsend(parallelSize, MPI_DOUBLE);
+            }
+        }
+    }
+};
+
+}  // namespace Eacpp
+
+int main(int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    std::string moeadName;
+    std::string parameterFilePath;
+    if (argc < 2 || argc > 4) {
+        RANK0(std::cerr << "Usage: " << argv[0] << " <moeadName> [parameterFilePath]" << std::endl;)
+        MPI_Finalize();
+        return 1;
+    } else if (argc == 2) {
+        moeadName = argv[1];
+    } else if (argc == 3) {
+        moeadName = argv[1];
+        parameterFilePath = argv[2];
+    }
+
+    auto benchmark = parameterFilePath.empty() ? Eacpp::ParallelMoeadBenchmark(moeadName)
+                                               : Eacpp::ParallelMoeadBenchmark(moeadName, parameterFilePath);
+    benchmark.Run();
+
+    MPI_Finalize();
+    return 0;
+}
