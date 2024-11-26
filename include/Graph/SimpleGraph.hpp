7daca531228ff1418809dadaccaf4cf32dd87fb0
hiro99988 <hrk9230.h@gmail.com>
2024-11-26 12:23:41 +0900

    いろいろ便利に

--- include/Graph/SimpleGraph.hpp
+++ include/Graph/SimpleGraph.hpp
@@ -9,6 +9,7 @@ namespace Eacpp {
 template <typename T>
 class SimpleGraph {
    public:
+    SimpleGraph() : _nodesNum(0), _matrix() {}
     SimpleGraph(int nodesNum) : _nodesNum(nodesNum), _matrix(ElementsNum(nodesNum), 0) {}
     SimpleGraph(int nodesNum, const std::vector<T>& matrix) : _nodesNum(nodesNum), _matrix(matrix) {
         if (matrix.size() != ElementsNum(nodesNum)) {
@@ -16,12 +17,26 @@ class SimpleGraph {
         }
     }
 
+    static size_t ElementsNum(int nodesNum);
+    static SimpleGraph<T> GnpRandomGraph(int nodesNum, double probability);
+
     typename std::vector<T>::reference operator()(int row, int col);
     typename std::vector<T>::const_reference operator()(int row, int col) const;
 
-    const std::vector<T>& Matrix() const {
-        return _matrix;
+    typename std::vector<T>::reference operator[](size_t index);
+    typename std::vector<T>::const_reference operator[](size_t index) const;
+    typename std::vector<T>::reference operator()(size_t row, size_t col);
+    typename std::vector<T>::const_reference operator()(size_t row, size_t col) const;
+
+    int size() const {
+        return _matrix.size();
     }
+
+    int NodesNum() const {
+        return _nodesNum;
+    }
+
+    void resize(int nodesNum);
 
     /// @brief Calculate the length of the shortest path from the specified start node to the end node.
     /// @param start The index of the start node.
@@ -30,24 +45,23 @@ class SimpleGraph {
     int ShortestPathLength(int start, int end) const;
 
     int MaxDegree() const;
-    int AverageShortestPathLength() const;
+    double AverageShortestPathLength() const;
     std::vector<std::vector<int>> ToAdjacencyList() const;
     void TwoOpt(size_t parent1, size_t parent2, size_t child1, size_t child2);
+    std::vector<int> GetEdges(size_t node) const;
 
    private:
     int _nodesNum;
     std::vector<T> _matrix;
 
     size_t Index(size_t row, size_t col) const;
-    size_t ElementsNum(int nodesNum) const;
     T Element(size_t row, size_t col) const;
 
     template <typename... Args>
-        requires std::same_as<Args..., size_t>
     void ValidateIndexes(Args... indexes) const;
 
     void ValidateEdge(size_t row, size_t col) const;
-};
+};  // namespace Eacpp
 
 template class SimpleGraph<bool>;
 template class SimpleGraph<int>;
--- src/Graph/SimpleGraph.cpp
+++ src/Graph/SimpleGraph.cpp
@@ -1,14 +1,76 @@
 #include "Graph/SimpleGraph.hpp"
 
 #include <algorithm>
+#include <cmath>
 #include <deque>
+#include <random>
+#include <set>
 #include <stdexcept>
+#include <utility>
 #include <vector>
+
+#include "Rng/Rng.h"
 
 namespace Eacpp {
 
 template <typename T>
+size_t SimpleGraph<T>::ElementsNum(int nodesNum) {
+    return nodesNum * (nodesNum - 1) / 2;
+}
+
+template <typename T>
+SimpleGraph<T> SimpleGraph<T>::GnpRandomGraph(int nodesNum, double probability) {
+    if (probability < 0 || probability > 1) {
+        throw std::invalid_argument("Probability must be in the range [0, 1]");
+    }
+
+    if (probability <= 0) {
+        SimpleGraph<T> emptyGraph(nodesNum);
+        return emptyGraph;
+    }
+
+    if (probability >= 1) {
+        std::vector<T> matrix(ElementsNum(nodesNum), 1);
+        SimpleGraph<T> completeGraph(nodesNum, matrix);
+        return completeGraph;
+    }
+
+    Rng rng;
+    SimpleGraph<T> graph(nodesNum);
+
+    for (int i = 0; i < nodesNum; ++i) {
+        for (int j = i + 1; j < nodesNum; ++j) {
+            if (rng.Random() < probability) {
+                graph(i, j) = 1;
+            }
+        }
+    }
+
+    return graph;
+}
+
 typename std::vector<T>::reference SimpleGraph<T>::operator()(int row, int col) {
+
+template <typename T>
+typename std::vector<T>::reference SimpleGraph<T>::operator[](size_t index) {
+    if (index >= _matrix.size()) {
+        throw std::out_of_range("Index out of range " + std::to_string(index) + " >= " + std::to_string(_matrix.size()));
+    }
+
+    return _matrix[index];
+}
+
+template <typename T>
+typename std::vector<T>::const_reference SimpleGraph<T>::operator[](size_t index) const {
+    if (index >= _matrix.size()) {
+        throw std::out_of_range("Index out of range " + std::to_string(index) + " >= " + std::to_string(_matrix.size()));
+    }
+
+    return _matrix[index];
+}
+
+template <typename T>
+typename std::vector<T>::reference SimpleGraph<T>::operator()(size_t row, size_t col) {
     ValidateIndexes(row, col);
     ValidateEdge(row, col);
 
@@ -16,11 +78,17 @@ typename std::vector<T>::reference SimpleGraph<T>::operator()(int row, int col) 
 }
 
 template <typename T>
-typename std::vector<T>::const_reference SimpleGraph<T>::operator()(int row, int col) const {
+typename std::vector<T>::const_reference SimpleGraph<T>::operator()(size_t row, size_t col) const {
     ValidateIndexes(row, col);
     ValidateEdge(row, col);
 
     return _matrix[Index(row, col)];
+}
+
+template <typename T>
+void SimpleGraph<T>::resize(int nodesNum) {
+    _nodesNum = nodesNum;
+    _matrix.resize(ElementsNum(nodesNum), 0);
 }
 
 template <typename T>
@@ -71,8 +139,8 @@ int SimpleGraph<T>::MaxDegree() const {
 }
 
 template <typename T>
-int SimpleGraph<T>::AverageShortestPathLength() const {
-    int sum = 0;
+double SimpleGraph<T>::AverageShortestPathLength() const {
+    double sum = 0;
     int count = 0;
 
     for (int i = 0; i < _nodesNum; ++i) {
@@ -113,10 +181,25 @@ void SimpleGraph<T>::TwoOpt(size_t parent1, size_t child1, size_t parent2, size_
         throw std::invalid_argument("No duplicate nodes allowed");
     }
 
-    this[parent1, child1] = 0;
-    this[parent2, child2] = 0;
-    this[parent1, child2] = 1;
-    this[parent2, child1] = 1;
+    _matrix[Index(parent1, child1)] = 0;
+    _matrix[Index(parent2, child2)] = 0;
+    _matrix[Index(parent1, child2)] = 1;
+    _matrix[Index(parent2, child1)] = 1;
+}
+
+template <typename T>
+std::vector<int> SimpleGraph<T>::GetEdges(size_t node) const {
+    ValidateIndexes(node);
+
+    std::vector<int> edges;
+
+    for (int i = 0; i < _nodesNum; ++i) {
+        if (node != i && Element(node, i)) {
+            edges.push_back(i);
+        }
+    }
+
+    return edges;
 }
 
 template <typename T>
@@ -132,11 +215,6 @@ size_t SimpleGraph<T>::Index(size_t row, size_t col) const {
 }
 
 template <typename T>
-size_t SimpleGraph<T>::ElementsNum(int nodesNum) const {
-    return nodesNum * (nodesNum - 1) / 2;
-}
-
-template <typename T>
 T SimpleGraph<T>::Element(size_t row, size_t col) const {
     ValidateIndexes(row, col);
 
@@ -149,7 +227,6 @@ T SimpleGraph<T>::Element(size_t row, size_t col) const {
 
 template <typename T>
 template <typename... Args>
-    requires std::same_as<Args..., size_t>
 void SimpleGraph<T>::ValidateIndexes(Args... indexes) const {
     for (size_t index : {indexes...}) {
         if (index >= _nodesNum) {
