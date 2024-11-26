699136bf1750103ff42de3903bad353af51606e0
hiro99988 <hrk9230.h@gmail.com>
2024-11-26 12:24:24 +0900

    initalize以外作成

--- include/Graph/NetworkTopologySearch.hpp
+++ include/Graph/NetworkTopologySearch.hpp
@@ -1,38 +1,80 @@
 #pragma once
 
+#include <fstream>
 #include <vector>
 
 #include "Algorithms/MoeadInitializer.h"
 #include "Graph/SimpleGraph.hpp"
+#include "Rng/IRng.h"
+#include "Rng/Rng.h"
 
 namespace Eacpp {
 
 class NetworkTopologySearch {
    public:
-    NetworkTopologySearch(int objectivesNum, int neighborhoodSize, int idealHopsNumBetweenNeighbors,
-                          int idealHopsNumBetweenExtremesAndAnyNode, const std::vector<std::vector<int>>& neighborhood,
-                          const std::vector<int>& extremeNodes, const SimpleGraph<int>& graph)
+    NetworkTopologySearch(int objectivesNum, int neighborhoodSize, int divisionsNumOfWeightVector, int nodesNum,
+                          int idealPathLengthBetweenNeighbors, int idealPathLengthBetweenExtremesAndAnyNode, int repeats,
+                          double initialTemperature, double minTemperature, double coolingRate)
         : _objectivesNum(objectivesNum),
           _neighborhoodSize(neighborhoodSize),
-          _idealHopsNumBetweenNeighbors(idealHopsNumBetweenNeighbors),
-          _idealHopsNumBetweenExtremesAndAnyNode(idealHopsNumBetweenExtremesAndAnyNode),
-          _neighborhood(neighborhood),
-          _extremeNodes(extremeNodes),
-          _graph(graph) {}
+          _divisionsNumOfWeightVector(divisionsNumOfWeightVector),
+          _nodesNum(nodesNum),
+          _idealPathLengthBetweenNeighbors(idealPathLengthBetweenNeighbors),
+          _idealPathLengthBetweenExtremesAndAnyNode(idealPathLengthBetweenExtremesAndAnyNode),
+          _repeats(repeats),
+          _initialTemperature(initialTemperature),
+          _minTemperature(minTemperature),
+          _coolingRate(coolingRate),
+          _moeadInitializer(MoeadInitializer()),
+          _rng(std::make_unique<Rng>()) {}
 
-    void Search() const;
+    // NetworkTopologySearch(int objectivesNum, int neighborhoodSize, int divisionsNumOfWeightVector, int nodesNum,
+    //                       int idealPathLengthBetweenNeighbors, int idealPathLengthBetweenExtremesAndAnyNode, int repeats,
+    //                       double initialTemperature, double minTemperature, double coolingRate, SimpleGraph<int>
+    //                       initialGraph)
+    //     : _objectivesNum(objectivesNum),
+    //       _neighborhoodSize(neighborhoodSize),
+    //       _divisionsNumOfWeightVector(divisionsNumOfWeightVector),
+    //       _nodesNum(nodesNum),
+    //       _idealPathLengthBetweenNeighbors(idealPathLengthBetweenNeighbors),
+    //       _idealPathLengthBetweenExtremesAndAnyNode(idealPathLengthBetweenExtremesAndAnyNode),
+    //       _repeats(repeats),
+    //       _initialTemperature(initialTemperature),
+    //       _minTemperature(minTemperature),
+    //       _coolingRate(coolingRate),
+    //       _moeadInitializer(MoeadInitializer()),
+    //       _rng(std::make_unique<Rng>()),
+    //       _bestSoFarGraph(initialGraph) {
+    //     _bestSoFarObjective = ComputeObjective(initialGraph);
+    // }
+
+    void Run();
+    void Initialize();
+    void Search();
+    void WriteAdjacencyListToCsv(const std::ofstream&) const;
 
    private:
     int _objectivesNum;
     int _neighborhoodSize;
-    int _idealHopsNumBetweenNeighbors;
-    int _idealHopsNumBetweenExtremesAndAnyNode;
+    int _divisionsNumOfWeightVector;
+    int _nodesNum;
+    int _idealPathLengthBetweenNeighbors;
+    int _idealPathLengthBetweenExtremesAndAnyNode;
+    int _repeats;
+    double _initialTemperature;
+    double _minTemperature;
+    double _coolingRate;
     std::vector<std::vector<int>> _neighborhood;
     std::vector<int> _extremeNodes;
-    SimpleGraph<int> _graph;
     MoeadInitializer _moeadInitializer;
+    std::unique_ptr<IRng> _rng;
+    double _bestSoFarObjective;
+    SimpleGraph<int> _bestSoFarGraph;
 
-    void InitializeNeighborhood() const;
+    void InitializeNeighborhood();
+    double ComputeObjective(const SimpleGraph<int>& graph) const;
+    bool AcceptanceCriterion(double newObjective, double oldObjective, double temperature) const;
+    void UpdateTemperature(double& temperature) const;
 };
 
 }  // namespace Eacpp
--- src/Graph/NetworkTopologySearch.cpp
+++ src/Graph/NetworkTopologySearch.cpp
@@ -1,67 +1,127 @@
 #include "Graph/NetworkTopologySearch.hpp"
 
 #include <algorithm>
+#include <stdexcept>
 #include <vector>
 
 #include "Algorithms/MoeadInitializer.h"
+#include "Graph/SimpleGraph.hpp"
+#include "Utils/MpiUtils.h"
 
 namespace Eacpp {
 
-void NetworkTopologySearch::Search() const {
-    InitializeNeighborhood();
-
-    std::vector<int> distances(_graph.NodesNum(), -1);
-    std::vector<int> previous(_graph.NodesNum(), -1);
-
-    std::queue<int> queue;
-    queue.push(_extremeNodes[0]);
-    distances[_extremeNodes[0]] = 0;
-
-    while (!queue.empty()) {
-        int node = queue.front();
-        queue.pop();
-
-        for (int i = 0; i < _neighborhoodSize; ++i) {
-            int neighbor = _neighborhood[node][i];
-
-            if (distances[neighbor] == -1) {
-                distances[neighbor] = distances[node] + 1;
-                previous[neighbor] = node;
-                queue.push(neighbor);
-            }
-        }
-    }
-
-    int current = _extremeNodes[1];
-    std::vector<int> path;
-
-    while (current != -1) {
-        path.push_back(current);
-        current = previous[current];
-    }
-
-    std::reverse(path.begin(), path.end());
-
-    for (int node : path) {
-        std::cout << node << ' ';
-    }
-
-    std::cout << std::endl;
+void NetworkTopologySearch::Run() {
+    Initialize();
+    Search();
 }
 
-void NetworkTopologySearch::InitializeNeighborhood() const {
-    std::vector<std::vector<int>> neighborhoods;
-    neighborhoods = _moeadInitializer.CalculateNeighborhoods2d(
-        _neighborhoodSize, _moeadInitializer.GenerateWeightVectors(_neighborhoodSize, _objectivesNum));
+void NetworkTopologySearch::Initialize() {
+    InitializeNeighborhood();
+    auto initialGraph = SimpleGraph<int>::GnpRandomGraph(_nodesNum, 0.2);
+    _bestSoFarObjective = ComputeObjective(initialGraph);
+    _bestSoFarGraph = initialGraph;
+}
 
-    for (int i = 0; i < neighborhoods.size(); ++i) {
-        for (int j = 0; j < neighborhoods[i].size(); ++j) {
-            if (neighborhoods[i][j] == i) {
-                neighborhoods[i].erase(neighborhoods[i].begin() + j);
-                break;
+void NetworkTopologySearch::Search() {
+    double bestObjective = _bestSoFarObjective;
+    auto bestNeighbor = _bestSoFarGraph;
+    double temperature = _initialTemperature;
+    for (int i = 0; i < _repeats; i++) {
+        auto neighbor = bestNeighbor;
+
+        double r = _rng->Random();
+        if (r < 0.5) {
+            auto index = _rng->Integer(neighbor.size() - 1);
+            neighbor[index] = !neighbor[index];
+        } else {
+            auto indexes = _rng->Integers(0, neighbor.NodesNum() - 1, 2, false);
+            auto child1 = _rng->Choice(neighbor.GetEdges(indexes[0]), 1, false);
+            auto child2 = _rng->Choice(neighbor.GetEdges(indexes[1]), 1, false);
+            while (child1[0] == indexes[1] || child2[0] == indexes[0] || child1[0] == child2[0]) {
+                child1 = _rng->Choice(neighbor.GetEdges(indexes[0]), 1, false);
+                child2 = _rng->Choice(neighbor.GetEdges(indexes[1]), 1, false);
+            }
+
+            neighbor.TwoOpt(indexes[0], indexes[1], child1[0], child2[0]);
+        }
+
+        double objective = ComputeObjective(neighbor);
+        if (AcceptanceCriterion(objective, bestObjective, temperature)) {
+            bestObjective = objective;
+            bestNeighbor = neighbor;
+            if (bestObjective < _bestSoFarObjective) {
+                _bestSoFarObjective = bestObjective;
+                _bestSoFarGraph = bestNeighbor;
+            }
+
+            // 表示
+            std::cout << "i: " << i << " Objective: " << bestObjective << " maxDegree: " << bestNeighbor.MaxDegree()
+                      << " temperature: " << temperature << std::endl;
+        }
+
+        UpdateTemperature(temperature);
+    }
+}
+
+void NetworkTopologySearch::InitializeNeighborhood() {
+    // int populationSize = _moeadInitializer.CalculatePopulationSize(_divisionsNumOfWeightVector, _objectivesNum);
+    // auto weightVectors = _moeadInitializer.GenerateWeightVectors(_objectivesNum, _neighborhoodSize);
+    // auto neighborhood = _moeadInitializer.CalculateNeighborhoods2d(_neighborhoodSize, weightVectors);
+    // auto workloads = CalculateNodeWorkloads(populationSize, _nodesNum);
+
+    // auto allNodeIndexes = GenerateAllNodeIndexes(populationSize, _nodesNum);
+
+    // // 近傍を計算
+    // _neighborhood.reverse(_nodesNum);
+
+    _neighborhood = std::vector<std::vector<int>>(_nodesNum);
+    _neighborhood[0] = {1, 2, 3};
+    _neighborhood[1] = {0, 2, 3};
+    _neighborhood[_nodesNum - 2] = {46, 47, 49};
+    _neighborhood[_nodesNum - 1] = {46, 47, 48};
+    for (int i = 2; i < _nodesNum - 2; i++) {
+        _neighborhood[i] = {i - 2, i - 1, i + 1, i + 2};
+    }
+
+    _extremeNodes = {0, 49};
+}
+
+double NetworkTopologySearch::ComputeObjective(const SimpleGraph<int>& graph) const {
+    // 近傍のノード間の最短経路長が理想値より大きい場合と極点ノードと任意のノード間の最短経路長が理想値より大きい場合の違反数を計算
+    int violationsNumNeighborhood = 0;
+    int violationsNumExtremes = 0;
+
+    for (int i = 0; i < graph.NodesNum(); i++) {
+        for (auto&& j : _neighborhood[i]) {
+            if (i == j) {
+                continue;
+            }
+
+            double shortestPathLength = graph.ShortestPathLength(i, j);
+            if (shortestPathLength > _idealPathLengthBetweenNeighbors) {
+                ++violationsNumNeighborhood;
+            }
+        }
+
+        for (int j = 0; j < _extremeNodes.size(); j++) {
+            double shortestPathLength = graph.ShortestPathLength(i, _extremeNodes[j]);
+            if (shortestPathLength > _idealPathLengthBetweenExtremesAndAnyNode) {
+                ++violationsNumExtremes;
             }
         }
     }
+
+    return graph.MaxDegree() + violationsNumNeighborhood + violationsNumExtremes;
+}
+
+bool NetworkTopologySearch::AcceptanceCriterion(double newObjective, double oldObjective, double temperature) const {
+    double delta = newObjective - oldObjective;
+    return delta < 0 || _rng->Random() < std::exp(-delta / temperature);
+}
+
+void NetworkTopologySearch::UpdateTemperature(double& temperature) const {
+    temperature *= _coolingRate;
+    temperature = std::max(temperature, _minTemperature);
 }
 
 }  // namespace Eacpp
