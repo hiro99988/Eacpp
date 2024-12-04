d3cc9aa47fc9256d006c4eb9d6243daf78d79a7c
hiro99988 <hrk9230.h@gmail.com>
2024-12-05 06:30:41 +0900

    jsonからパラメータを読み込むように

---
+++ examples/GraphGenerator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <set>
+#include <vector>
+
+#include "Graph/NetworkTopologySearch.hpp"
+#include "Graph/SimpleGraph.hpp"
+
+using namespace Eacpp;
+
+int main() {
+    int objectivesNum;
+    int neighborhoodSize;
+    int divisionsNumOfWeightVector;
+    int nodesNum;
+    int degree;
+    int idealPathLengthBetweenNeighbors;
+    int idealPathLengthBetweenExtremesAndAnyNode;
+    int repeats;
+    double initialTemperature;
+    double minTemperature;
+    double coolingRate;
+
+    nlohmann::json json = nlohmann::json::parse("data/inputs/graphParameters.json");
+
+    objectivesNum = json["objectivesNum"];
+    neighborhoodSize = json["neighborhoodSize"];
+    divisionsNumOfWeightVector = json["divisionsNumOfWeightVector"];
+    nodesNum = json["nodesNum"];
+    degree = json["degree"];
+    idealPathLengthBetweenNeighbors = json["idealPathLengthBetweenNeighbors"];
+    idealPathLengthBetweenExtremesAndAnyNode = json["idealPathLengthBetweenExtremesAndAnyNode"];
+    repeats = json["repeats"];
+    initialTemperature = json["initialTemperature"];
+    minTemperature = json["minTemperature"];
+    coolingRate = json["coolingRate"];
+
+    NetworkTopologySearch search(objectivesNum, neighborhoodSize, divisionsNumOfWeightVector, nodesNum, degree,
+                                 idealPathLengthBetweenNeighbors, idealPathLengthBetweenExtremesAndAnyNode);
+
+    search.Run(repeats, initialTemperature, minTemperature, coolingRate);
+
+    return 0;
+}
