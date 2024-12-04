db75a7b603389cac58b6257e4f7fc0ffc1393c91
hiro99988 <hrk9230.h@gmail.com>
2024-12-05 06:25:23 +0900

    更新

--- examples/GraphEx.cpp
+++ examples/GraphEx.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
 #include "Graph/NetworkTopologySearch.hpp"
 #include "Graph/SimpleGraph.hpp"
 
@@ -6,21 +10,20 @@ using namespace Eacpp;
 int main() {
     int objectivesNum = 2;
     int neighborhoodSize = 21;
-    int divisionsNumOfWeightVector = 300;
+    int divisionsNumOfWeightVector = 299;
     int nodesNum = 50;
+    int degree = 4;
     int idealPathLengthBetweenNeighbors = 2;
-    int idealPathLengthBetweenExtremesAndAnyNode = 3;
-    int repeats = 100000;
+    int idealPathLengthBetweenExtremesAndAnyNode = 4;
+    int repeats = 1'000'000;
     double initialTemperature = 1000;
-    double minTemperature = 0.01;
-    double coolingRate = 0.9993;
+    double minTemperature = 0.001;
+    double coolingRate = 0.995;
 
-    NetworkTopologySearch search(objectivesNum, neighborhoodSize, divisionsNumOfWeightVector, nodesNum,
-                                 idealPathLengthBetweenNeighbors, idealPathLengthBetweenExtremesAndAnyNode, repeats,
-                                 initialTemperature, minTemperature, coolingRate);
+    NetworkTopologySearch search(objectivesNum, neighborhoodSize, divisionsNumOfWeightVector, nodesNum, degree,
+                                 idealPathLengthBetweenNeighbors, idealPathLengthBetweenExtremesAndAnyNode);
 
-    search.Initialize();
-    search.Search();
+    search.Run(repeats, initialTemperature, minTemperature, coolingRate);
 
     return 0;
 }
