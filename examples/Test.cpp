89b5801be6878c37ad52ae0fb49f129e2185cbbd
hiro99988 <hrk9230.h@gmail.com>
2025-05-15 16:14:08 +0900

    変更

--- examples/Test.cpp
+++ examples/Test.cpp
@@ -1,6 +1,5 @@
-#include <pybind11/embed.h>
-
 #include <Eigen/Core>
+#include <toml.hpp>
 
 #include "Individual.h"
 #include "Problems/Hpa.hpp"
@@ -9,32 +8,14 @@ using namespace Eacpp;
 namespace py = pybind11;
 
 int main(int argc, char* argv[]) {
-    // Pythonインタプリタを初期化
-    py::scoped_interpreter guard{};
-    // Pythonモジュールをインポート
-    py::module module = py::module::import("extern.hpa.hpa.problem");
+    constexpr const char* config_file = "data/inputs/benchmarkParameter.toml";
 
-    // コマンドライン引数から問題名を取得
-    std::string problemName = "HPA201";
-    int level = 0;
-    if (argc == 2) {
-        problemName = argv[1];
-    } else if (argc == 3) {
-        problemName = argv[1];
-        level = std::stoi(argv[2]);
-    }
-
-    Hpa hpa(module, problemName.c_str(), 4, level);
-
-    Eigen::ArrayXd x(hpa.DecisionVariablesNum());
-    x.setLinSpaced(x.size(), 0.0, 1.0);
-    Individuald individual(x);
-    hpa.ComputeObjectiveSet(individual);
-
-    std::cout << "Decision Variables: " << individual.solution.transpose()
-              << std::endl;
-    std::cout << "Objectives: " << individual.objectives.transpose()
-              << std::endl;
+    // Load the TOML configuration file
+    auto config = toml::parse(config_file);
+    int trial = toml::find<int>(config, "trial");
+    int generationNum = toml::find<int>(config, "generation_num");
+    std::cout << "trial: " << trial << std::endl;
+    std::cout << "generationNum: " << generationNum << std::endl;
 
     return 0;
 }
